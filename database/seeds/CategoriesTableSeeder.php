d52c542481c02d9dff5a9453c4d693da71a128f4
bestmomo <colorstonic@free.fr>
2017-11-08 18:55:20 +0100

    Update seeder

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -8,30 +8,23 @@ class CategoriesTableSeeder extends Seeder
 
     public function run()
     {
-        Category::create([
+        Category::insert([
+        [
             'name' => 'Paysages',
             'slug' => 'paysages',
-        ]);
-
-        Category::create([
+        ],[
             'name' => 'Maisons',
             'slug' => 'maisons',
-        ]); 
-
-        Category::create([
+        ],[
             'name' => 'Personnages',
             'slug' => 'personnages',
-        ]); 
-
-        Category::create([
+        ],[
             'name' => 'Animaux',
             'slug' => 'animaux',
-        ]); 
-
-        Category::create([
+        ],[
             'name' => 'Végétation',
             'slug' => 'vegetation',
-        ]);
+        ]]);
 
     }
 }
